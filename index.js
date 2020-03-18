294c29860b1fd99ab525c7d1830c916c8acb1f4d
DanielJSottile <danielsottile@gmail.com>
2020-03-18 14:49:29 -0400

    fizzbuzz

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// your code here
+'use strict';
+function fizzBuzz(){
+  $('#number-chooser').submit(event => {
+    event.preventDefault();
+    const num = parseInt($(event.currentTarget).find('input[name="number-choice"]').val());
+    
+    let result = [];
+
+    for (let i = 1; i <= num; i++) {
+      if (i % 15 === 0) {
+        result.push($('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>')); 
+      } else if (i % 5 === 0) {
+        result.push($('<div class="fizz-buzz-item buzz"><span>buzz</span></div>'));
+      } else if (i % 3 === 0) {
+        result.push($('<div class="fizz-buzz-item fizz"><span>fizz</span></div>')); 
+      } else {
+        result.push($(`<div class="fizz-buzz-item"><span>${i}</span></div>`));
+      }
+      $('.js-results').append(result);
+    }
+  });
+}
+
+$(fizzBuzz);
